fb1ffc92b0ef0d5b6329fd375c6f65e11768ac6f
jrry <jerry017@gmail.com>
2018-01-13 23:31:02 +0100

    New solution

---
+++ TapeEquilibrium.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] A) {
+        int sum = 0;
+        
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+        }
+
+        int left = A[0];
+        int right = sum - left;
+        int abs = Math.abs(left - right);
+        int diff = abs;
+
+        for (int i = 1; i < A.length - 1; i++) {
+            left += A[i];
+            right = sum - left;
+            abs = Math.abs(left - right);
+
+            if (abs < diff) {
+                diff = abs;
+            }
+        }
+        return diff;
+    }
+}
